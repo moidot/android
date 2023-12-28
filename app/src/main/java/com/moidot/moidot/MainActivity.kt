3595dd849d0a2b50d1684eac0eeab3cf87703f22
kite-aswemake <stopkite98@gmail.com>
2023-12-28 09:36:32 +0900

    #2 feat: BaseActivity, BaseFragment 생성

--- app/src/main/java/com/moidot/moidot/MainActivity.kt
+++ app/src/main/java/com/moidot/moidot/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.moidot.moidot
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.moidot.moidot.databinding.ActivityMainBinding
+import com.moidot.moidot.presentation.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
