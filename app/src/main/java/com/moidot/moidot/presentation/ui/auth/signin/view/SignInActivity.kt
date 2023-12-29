48977f995d7cb528800e0dcdfaf894d521bc883d
stopkite <stopkite98@gmail.com>
2023-12-30 02:56:46 +0900

    #10 fix: 로그인 도메인 명 서버와 통일

---
+++ app/src/main/java/com/moidot/moidot/presentation/ui/auth/signin/view/SignInActivity.kt
@@ -0,0 +1,8 @@
+package com.moidot.moidot.presentation.ui.auth.signin.view
+
+import com.moidot.moidot.R
+import com.moidot.moidot.databinding.ActivitySigninBinding
+import com.moidot.moidot.presentation.ui.base.BaseActivity
+
+class SignInActivity : BaseActivity<ActivitySigninBinding>(R.layout.activity_signin) {
+}
