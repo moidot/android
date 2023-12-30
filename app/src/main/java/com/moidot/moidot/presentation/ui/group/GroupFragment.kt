717ed3dbfaba8170b755cb834f9219f0aa69c3e4
stopkite <stopkite98@gmail.com>
2023-12-30 13:20:00 +0900

    #12 feat: 메인 탭 fragment 추가

---
+++ app/src/main/java/com/moidot/moidot/presentation/ui/group/GroupFragment.kt
@@ -0,0 +1,8 @@
+package com.moidot.moidot.presentation.ui.group
+
+import com.moidot.moidot.R
+import com.moidot.moidot.databinding.FragmentGroupBinding
+import com.moidot.moidot.presentation.ui.base.BaseFragment
+
+class GroupFragment : BaseFragment<FragmentGroupBinding>(R.layout.fragment_group) {
+}
