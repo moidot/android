4de87195652f044c4de2a9154bedcd72337a9bde
stopkite <stopkite98@gmail.com>
2024-01-02 01:06:21 +0900

    #14 feat: 두번째 온보딩 화면 생성 및 로직 구현

---
+++ app/src/main/java/com/moidot/moidot/data/data/OnboardItem.kt
@@ -0,0 +1,9 @@
+package com.moidot.moidot.data.data
+
+import android.graphics.drawable.Drawable
+
+data class OnboardItem(
+    val img: Drawable,
+    val title: String,
+    val content: String
+)
--- app/src/main/java/com/moidot/moidot/presentation/ui/onboard/view/OnboardSecondFragment.kt
+++ app/src/main/java/com/moidot/moidot/presentation/ui/onboard/view/OnboardSecondFragment.kt
@@ -2,23 +2,77 @@ package com.moidot.moidot.presentation.ui.onboard.view
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.activityViewModels
+import androidx.viewpager2.widget.ViewPager2
 import com.moidot.moidot.R
+import com.moidot.moidot.data.data.OnboardItem
 import com.moidot.moidot.databinding.FragmentOnboardSecondBinding
 import com.moidot.moidot.presentation.ui.base.BaseFragment
-import com.moidot.moidot.presentation.ui.util.StatusBarColorUtil
-import com.moidot.moidot.presentation.ui.util.StatusBarColorUtil.Companion.DARK_ICON_COLOR
+import com.moidot.moidot.presentation.ui.onboard.adapter.OnBoardingAdapter
+import com.moidot.moidot.presentation.ui.onboard.viewmodel.OnboardViewModel
+import com.moidot.moidot.presentation.util.StatusBarColorUtil
+import com.moidot.moidot.presentation.util.StatusBarColorUtil.Companion.DARK_ICON_COLOR
 
 class OnboardSecondFragment : BaseFragment<FragmentOnboardSecondBinding>(R.layout.fragment_onboard_second) {
+
+    private val viewModel: OnboardViewModel by activityViewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initBinding()
         initView()
+        setupObservers()
+    }
+
+    private fun initBinding() {
+        binding.fragment = this
+        binding.activity = (activity as OnboardActivity)
     }
 
     private fun initView() {
         initStatusBar()
+        setOnBoardVpAdapter()
+        setOnBoardVpEvent()
     }
 
     private fun initStatusBar() {
         StatusBarColorUtil(requireActivity()).setStatusBar(R.color.white, DARK_ICON_COLOR)
+    }
+
+    private fun setOnBoardItems():MutableList<OnboardItem> {
+        return mutableListOf<OnboardItem>().apply {
+            add(OnboardItem(ContextCompat.getDrawable(requireContext(),R.drawable.img_onboard_second_door)!!,resources.getString(R.string.onboard_second_title_social_login),resources.getString(R.string.onboard_second_content_social_login)))
+            add(OnboardItem(ContextCompat.getDrawable(requireContext(),R.drawable.img_login_logo)!!,resources.getString(R.string.onboard_second_title_moidot_space),resources.getString(R.string.onboard_second_content_moidot_space)))
+            add(OnboardItem(ContextCompat.getDrawable(requireContext(),R.drawable.img_login_logo)!!,resources.getString(R.string.onboard_second_title_invite),resources.getString(R.string.onboard_second_content_invite)))
+            add(OnboardItem(ContextCompat.getDrawable(requireContext(),R.drawable.img_login_logo)!!,resources.getString(R.string.onboard_second_title_recommend_place),resources.getString(R.string.onboard_second_content_recommend_place)))
+            add(OnboardItem(ContextCompat.getDrawable(requireContext(),R.drawable.img_login_logo)!!,resources.getString(R.string.onboard_second_title_vote_place),resources.getString(R.string.onboard_second_content_vote_place)))
+        }
+    }
+
+    private fun setOnBoardVpAdapter() {
+        val onBoardingAdapter = OnBoardingAdapter()
+        onBoardingAdapter.itemList = setOnBoardItems()
+        binding.fgOnboardSecondVp.adapter = onBoardingAdapter
+    }
+
+    private fun setOnBoardVpEvent() {
+        binding.fgOnboardSecondVp.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                viewModel.setCurrentPos(position)
+            }
+        })
+    }
+
+    private fun setupObservers() {
+        viewModel.currentPos.observe(viewLifecycleOwner) {
+            val indicatorNumber = it.plus(1)
+        }
+    }
+
+    fun moveToNext() {
+        val nextPos = viewModel.currentPos.value!!.plus(1)
+        if (nextPos < 5) binding.fgOnboardSecondVp.currentItem = nextPos else (activity as OnboardActivity).exitOnboard()
     }
 }
