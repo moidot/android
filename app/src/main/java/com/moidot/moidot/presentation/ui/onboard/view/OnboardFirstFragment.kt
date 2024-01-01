3f1692bae47040ed964e691db9f5f1273dcf7e60
stopkite <stopkite98@gmail.com>
2024-01-01 22:20:44 +0900

    #14 feat: 첫번째 온보딩 로직 생성

--- app/src/main/java/com/moidot/moidot/presentation/ui/onboard/view/OnboardFirstFragment.kt
+++ app/src/main/java/com/moidot/moidot/presentation/ui/onboard/view/OnboardFirstFragment.kt
@@ -5,22 +5,27 @@ import android.view.View
 import com.moidot.moidot.R
 import com.moidot.moidot.databinding.FragmentOnboardFirstBinding
 import com.moidot.moidot.presentation.ui.base.BaseFragment
-import com.moidot.moidot.presentation.ui.util.StatusBarColorUtil
-import com.moidot.moidot.presentation.ui.util.StatusBarColorUtil.Companion.LIGHT_ICON_COLOR
+import com.moidot.moidot.presentation.util.StatusBarColorUtil
+import com.moidot.moidot.presentation.util.StatusBarColorUtil.Companion.LIGHT_ICON_COLOR
 
 class OnboardFirstFragment : BaseFragment<FragmentOnboardFirstBinding>(R.layout.fragment_onboard_first) {
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initView()
-    }
-
-    private fun initView() {
-       initStatusBar()
+        binding.fragment = this
+        initStatusBar()
     }
 
     private fun initStatusBar() {
         StatusBarColorUtil(requireActivity()).setStatusBar(R.color.gray800, LIGHT_ICON_COLOR)
     }
 
+    fun moveToOnboardSecond() {
+        (activity as OnboardActivity).supportFragmentManager.beginTransaction()
+            .replace(R.id.onboard_fcv, OnboardSecondFragment())
+            .commit()
+    }
+
+    fun exitOnboard() {
+        (activity as OnboardActivity).finish()
+    }
 }
