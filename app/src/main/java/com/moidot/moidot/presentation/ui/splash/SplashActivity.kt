83cf5b5add3624db1b310adb21c74bc71d885662
stopkite <stopkite98@gmail.com>
2024-01-03 14:55:23 +0900

    #5 ui: custom splash activity 추가

---
+++ app/src/main/java/com/moidot/moidot/presentation/ui/splash/SplashActivity.kt
@@ -0,0 +1,8 @@
+package com.moidot.moidot.presentation.ui.splash
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+}
