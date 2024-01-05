3edf24f39d6f3194b05176a1f5f704372c11b79e
stopkite <stopkite98@gmail.com>
2024-01-05 21:53:33 +0900

    #21 feat: 온보딩 읽음 상태값 저장 코드 추가

--- app/src/main/java/com/moidot/moidot/presentation/ui/onboard/viewmodel/OnboardViewModel.kt
+++ app/src/main/java/com/moidot/moidot/presentation/ui/onboard/viewmodel/OnboardViewModel.kt
@@ -1,14 +1,14 @@
 package com.moidot.moidot.presentation.ui.onboard.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.moidot.moidot.domain.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class OnboardViewModel @Inject constructor() : ViewModel() {
+class OnboardViewModel @Inject constructor(private val userRepository: UserRepository) : ViewModel() {
 
-    // TODO 상태값 저장
-    fun saveOnboardDonePrefs() {
-
+    fun saveOnboardDone() {
+        userRepository.saveOnboardDoneState(true)
     }
 }
