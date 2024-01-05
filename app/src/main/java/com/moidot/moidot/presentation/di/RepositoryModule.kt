c6612ed369d8451445ae205fb01a0d0f8d32d6eb
stopkite <stopkite98@gmail.com>
2024-01-05 21:51:00 +0900

    #21 feat: datasource, repository 주입

--- app/src/main/java/com/moidot/moidot/presentation/di/RepositoryModule.kt
+++ app/src/main/java/com/moidot/moidot/presentation/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.moidot.moidot.presentation.di
 
 import com.moidot.moidot.data.repository.AuthRepositoryImpl
+import com.moidot.moidot.data.repository.UserRepositoryImpl
 import com.moidot.moidot.domain.repository.AuthRepository
+import com.moidot.moidot.domain.repository.UserRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -12,6 +14,10 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindUserRepository(userRepositoryImpl: UserRepositoryImpl): UserRepository
 
     @Binds
     @Singleton
