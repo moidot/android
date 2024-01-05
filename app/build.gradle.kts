61dbe33c9c31650fa94ab2e40bbb50fa5cd0e5b3
stopkite <stopkite98@gmail.com>
2024-01-05 17:01:19 +0900

    #19 chore: naver sdk import 및 초기화

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,8 +58,6 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation(files("/Users/kite/AndroidStudioProjects/moidot/app/libs/oauth-5.9.0.aar"))
-    implementation(files("libs/oauth-5.9.0.arr"))
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -95,6 +93,9 @@ dependencies {
 
     // Kakao
     implementation("com.kakao.sdk:v2-user:2.19.0") // signin
+
+    // Naver
+    implementation("com.navercorp.nid:oauth:5.9.0")
 }
 
 kapt {
--- app/src/main/java/com/moidot/moidot/presentation/ui/MoidotApplication.kt
+++ app/src/main/java/com/moidot/moidot/presentation/ui/MoidotApplication.kt
@@ -3,6 +3,10 @@ package com.moidot.moidot.presentation.ui
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
 import com.moidot.moidot.BuildConfig.KAKAO_NATIVE_APP_KEY
+import com.moidot.moidot.BuildConfig.NAVER_CLIENT_ID
+import com.moidot.moidot.BuildConfig.NAVER_CLIENT_SECRET_KEY
+import com.moidot.moidot.R
+import com.navercorp.nid.NaverIdLoginSDK
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -15,6 +19,7 @@ class MoidotApplication : Application() {
 
     private fun initSdk() {
         KakaoSdk.init(this, KAKAO_NATIVE_APP_KEY)
+        NaverIdLoginSDK.initialize(this, NAVER_CLIENT_ID, NAVER_CLIENT_SECRET_KEY, getString(R.string.app_name))
     }
 
     companion object {
