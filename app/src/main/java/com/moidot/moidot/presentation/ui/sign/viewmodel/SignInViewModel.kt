ca8c61946b7667b4562c017ce3b86983b44ccecf
stopkite <stopkite98@gmail.com>
2024-01-05 21:57:06 +0900

    #21 feat: tokens 저장 코드 추가

--- app/src/main/java/com/moidot/moidot/presentation/ui/sign/viewmodel/SignInViewModel.kt
+++ app/src/main/java/com/moidot/moidot/presentation/ui/sign/viewmodel/SignInViewModel.kt
@@ -6,21 +6,33 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.moidot.moidot.data.remote.response.ResponseSignIn
 import com.moidot.moidot.domain.repository.AuthRepository
+import com.moidot.moidot.domain.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class SignInViewModel @Inject constructor(private val authRepository: AuthRepository) : ViewModel() {
+class SignInViewModel @Inject constructor(
+    private val userRepository: UserRepository,
+    private val authRepository: AuthRepository
+) : ViewModel() {
 
     private val _signInResult = MutableLiveData<ResponseSignIn.Data>()
-    val signInResult : LiveData<ResponseSignIn.Data> = _signInResult
+    val signInResult: LiveData<ResponseSignIn.Data> = _signInResult
 
     fun signInWithSocialToken(token: String, platform: String) {
         viewModelScope.launch { // TODO refresh 토큰 및 실패 처리
             authRepository.signIn(token, platform).onSuccess {
                 _signInResult.value = it.data
+                saveUserTokens(it.data.accessToken, it.data.refreshToken)
             }
+        }
+    }
+
+    private fun saveUserTokens(accessToken: String, refreshToken: String) {
+        userRepository.apply {
+            saveAccessToken(accessToken)
+            saveRefreshToken(refreshToken)
         }
     }
 }
