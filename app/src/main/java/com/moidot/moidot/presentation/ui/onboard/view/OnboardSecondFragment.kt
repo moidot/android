4e7ba848ef699656e60b4a5f0ddfcf8609f3834f
stopkite <stopkite98@gmail.com>
2024-01-06 16:37:02 +0900

    #26 ui: 이미지 에셋 추가 - group 없을 때 - onboard 세번째 이미지

--- app/src/main/java/com/moidot/moidot/presentation/ui/onboard/view/OnboardSecondFragment.kt
+++ app/src/main/java/com/moidot/moidot/presentation/ui/onboard/view/OnboardSecondFragment.kt
@@ -48,7 +48,7 @@ class OnboardSecondFragment : BaseFragment<FragmentOnboardSecondBinding>(R.layou
         return mutableListOf<OnboardItem>().apply {
             add(OnboardItem(ContextCompat.getDrawable(requireContext(),R.drawable.img_onboard_second_door)!!,resources.getString(R.string.onboard_second_title_social_sign_in),resources.getString(R.string.onboard_second_content_social_sign_in)))
             add(OnboardItem(ContextCompat.getDrawable(requireContext(),R.drawable.img_sign_in_logo)!!,resources.getString(R.string.onboard_second_title_moidot_space),resources.getString(R.string.onboard_second_content_moidot_space)))
-            add(OnboardItem(ContextCompat.getDrawable(requireContext(),R.drawable.img_sign_in_logo)!!,resources.getString(R.string.onboard_second_title_invite),resources.getString(R.string.onboard_second_content_invite)))
+            add(OnboardItem(ContextCompat.getDrawable(requireContext(),R.drawable.img_onboard_second_invite)!!,resources.getString(R.string.onboard_second_title_invite),resources.getString(R.string.onboard_second_content_invite)))
             add(OnboardItem(ContextCompat.getDrawable(requireContext(),R.drawable.img_sign_in_logo)!!,resources.getString(R.string.onboard_second_title_recommend_place),resources.getString(R.string.onboard_second_content_recommend_place)))
             add(OnboardItem(ContextCompat.getDrawable(requireContext(),R.drawable.img_sign_in_logo)!!,resources.getString(R.string.onboard_second_title_vote_place),resources.getString(R.string.onboard_second_content_vote_place)))
         }
