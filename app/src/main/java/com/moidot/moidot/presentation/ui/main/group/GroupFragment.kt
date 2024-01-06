d0263f53d8abc9a6d057e2be16cbcdab719cdb14
stopkite <stopkite98@gmail.com>
2024-01-06 23:50:45 +0900

    #26 feat: 내그룹 어댑터 생성

--- app/src/main/java/com/moidot/moidot/presentation/ui/main/group/GroupFragment.kt
+++ app/src/main/java/com/moidot/moidot/presentation/ui/main/group/GroupFragment.kt
@@ -1,9 +1,12 @@
 package com.moidot.moidot.presentation.ui.main.group
 
 import android.content.Context
+import android.os.Bundle
+import android.view.View
 import com.moidot.moidot.R
 import com.moidot.moidot.databinding.FragmentGroupBinding
 import com.moidot.moidot.presentation.ui.base.BaseFragment
+import com.moidot.moidot.presentation.ui.main.group.adater.MyGroupAdapter
 import com.moidot.moidot.presentation.util.StatusBarColorUtil
 import com.moidot.moidot.presentation.util.StatusBarColorUtil.Companion.DARK_ICON_COLOR
 import com.moidot.moidot.presentation.util.StatusBarColorUtil.Companion.LIGHT_ICON_COLOR
@@ -15,8 +18,19 @@ class GroupFragment : BaseFragment<FragmentGroupBinding>(R.layout.fragment_group
         setStatusBarState(STATUS_BAR_LIGHT)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initAdapter()
+    }
+
+    private fun initAdapter() {
+        val myGroupAdapter = MyGroupAdapter()
+        myGroupAdapter.submitList(listOf<String>("모이닷 팀 스페이스", "여기어때", "야놀자", "오늘의집", "모이닷 팀 스페이스", "오늘의집", "오늘의집", "네이버", "카카오", "다음", "쿠팡"))
+        binding.fgGroupRvMyGroup.adapter = myGroupAdapter
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
         setStatusBarState(STATUS_BAR_DARK)
     }
 
