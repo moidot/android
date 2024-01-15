fbeabfdfbcf5c44db534089e10e0e17d195fe69c
stopkite <stopkite98@gmail.com>
2024-01-16 02:06:54 +0900

    #31 ui: 텍스트 길이에 따른 뷰 변화

--- app/src/main/java/com/moidot/moidot/presentation/ui/main/group/create/view/InputGroupInfoFragment.kt
+++ app/src/main/java/com/moidot/moidot/presentation/ui/main/group/create/view/InputGroupInfoFragment.kt
@@ -1,9 +1,37 @@
 package com.moidot.moidot.presentation.ui.main.group.create.view
 
+import android.os.Bundle
+import android.view.View
+import androidx.core.widget.addTextChangedListener
+import androidx.fragment.app.activityViewModels
 import com.moidot.moidot.R
 import com.moidot.moidot.databinding.FragmentInputGroupInfoBinding
 import com.moidot.moidot.presentation.ui.base.BaseFragment
+import com.moidot.moidot.presentation.ui.main.group.create.viewmodel.CreateGroupViewModel
 
 class InputGroupInfoFragment : BaseFragment<FragmentInputGroupInfoBinding>(R.layout.fragment_input_group_info) {
 
+    private val viewModel: CreateGroupViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initBinding()
+        initView()
+    }
+
+    private fun initBinding() {
+        binding.viewModel = viewModel
+    }
+
+    private fun initView() {
+        setGroupNameTextChangeListener()
+    }
+
+    private fun setGroupNameTextChangeListener() {
+        binding.fgInputGroupInfoEtvGroupName.addTextChangedListener {
+            val name = it.toString()
+            viewModel.setGroupName(name)
+            viewModel.setGroupNameFieldActive(name)
+        }
+    }
 }
--- app/src/main/java/com/moidot/moidot/presentation/ui/main/group/create/viewmodel/CreateGroupViewModel.kt
+++ app/src/main/java/com/moidot/moidot/presentation/ui/main/group/create/viewmodel/CreateGroupViewModel.kt
@@ -1,7 +1,23 @@
 package com.moidot.moidot.presentation.ui.main.group.create.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
 class CreateGroupViewModel @Inject constructor() : ViewModel() {
+
+    private val _groupName = MutableLiveData<String>()
+    val groupName: LiveData<String> = _groupName
+
+    private val _isGroupNameFieldActive = MutableLiveData<Boolean>()
+    val isGroupNameFieldActive: LiveData<Boolean> = _isGroupNameFieldActive
+    fun setGroupName(name: String) {
+        _groupName.value = name
+    }
+
+    fun setGroupNameFieldActive(name: String) {
+        _isGroupNameFieldActive.value = name.isNotEmpty()
+    }
+
 }
