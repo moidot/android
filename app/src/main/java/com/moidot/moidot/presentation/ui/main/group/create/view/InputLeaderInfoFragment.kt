d0a37120ac78aea4883586865fbd09953b73d559
stopkite <stopkite98@gmail.com>
2024-01-16 16:38:00 +0900

    #33 feat: 모임이름 데이터 바인딩 연동

--- app/src/main/java/com/moidot/moidot/presentation/ui/main/group/create/view/InputLeaderInfoFragment.kt
+++ app/src/main/java/com/moidot/moidot/presentation/ui/main/group/create/view/InputLeaderInfoFragment.kt
@@ -1,9 +1,24 @@
 package com.moidot.moidot.presentation.ui.main.group.create.view
 
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.activityViewModels
 import com.moidot.moidot.R
 import com.moidot.moidot.databinding.FragmentInputLeaderInfoBinding
 import com.moidot.moidot.presentation.ui.base.BaseFragment
+import com.moidot.moidot.presentation.ui.main.group.create.viewmodel.CreateGroupViewModel
 
 class InputLeaderInfoFragment : BaseFragment<FragmentInputLeaderInfoBinding>(R.layout.fragment_input_leader_info) {
+
+    private val viewModel: CreateGroupViewModel by activityViewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initBinding()
+    }
+
+    private fun initBinding() {
+        binding.viewModel = viewModel
+    }
 
 }
