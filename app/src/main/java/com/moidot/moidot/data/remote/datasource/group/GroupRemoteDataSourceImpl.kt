bbaecb6356e9c04734fd928009bb406dea3baf82
stopkite <stopkite98@gmail.com>
2024-01-25 01:43:43 +0900

    #47 refactor: group datasource handler 적용

--- app/src/main/java/com/moidot/moidot/data/remote/datasource/group/GroupRemoteDataSourceImpl.kt
+++ app/src/main/java/com/moidot/moidot/data/remote/datasource/group/GroupRemoteDataSourceImpl.kt
@@ -1,25 +1,23 @@
 package com.moidot.moidot.data.remote.datasource.group
 
 import com.moidot.moidot.data.api.GroupService
+import com.moidot.moidot.data.remote.datasource.getResultFromResponse
 import com.moidot.moidot.data.remote.request.RequestCreateGroup
 import com.moidot.moidot.data.remote.response.ResponseCreateGroup
 import com.moidot.moidot.data.remote.response.ResponseParticipateGroup
+import com.moidot.moidot.data.remote.response.ResponseValidateNickname
 import javax.inject.Inject
 
-class GroupRemoteDataSourceImpl @Inject constructor(private val groupService: GroupService):GroupRemoteDataSource {
+class GroupRemoteDataSourceImpl @Inject constructor(private val groupService: GroupService) : GroupRemoteDataSource {
     override suspend fun getMyGroupList(): Result<ResponseParticipateGroup> {
-        val response = groupService.getParticipateGroup()
-        if (response.isSuccessful) {
-            response.body()?.let { return Result.success(it) }
-        }
-        return Result.failure(IllegalStateException(response.message()))
+        return groupService.getParticipateGroup().getResultFromResponse()
     }
 
     override suspend fun createGroup(requestCreateGroup: RequestCreateGroup): Result<ResponseCreateGroup> {
-        val response = groupService.createGroup(requestCreateGroup)
-        if (response.isSuccessful) {
-            response.body()?.let { return Result.success(it) }
-        }
-        return Result.failure(IllegalStateException(response.message()))
+        return groupService.createGroup(requestCreateGroup).getResultFromResponse()
+    }
+
+    override suspend fun validateNickname(groupId: Int, nickname: String): Result<ResponseValidateNickname> {
+        return groupService.validateNickname(groupId, nickname).getResultFromResponse()
     }
 }
