4d93feba98d66baf2e879456281a261c10a1105c
stopkite <stopkite98@gmail.com>
2024-01-26 00:59:05 +0900

    #50 feat: 모임장 네비게이션 이동로직 구현

--- app/src/main/java/com/moidot/moidot/presentation/ui/main/group/space/leader/LeaderSpaceActivity.kt
+++ app/src/main/java/com/moidot/moidot/presentation/ui/main/group/space/leader/LeaderSpaceActivity.kt
@@ -1,9 +1,55 @@
 package com.moidot.moidot.presentation.ui.main.group.space.leader
 
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.tabs.TabLayout
 import com.moidot.moidot.R
 import com.moidot.moidot.databinding.ActivityLeaderSpaceBinding
 import com.moidot.moidot.presentation.ui.base.BaseActivity
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LeaderSpaceActivity : BaseActivity<ActivityLeaderSpaceBinding>(R.layout.activity_leader_space) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initBinding()
+        initView()
+    }
+
+    private fun initBinding() {
+        binding.activity = this
+    }
+
+    private fun initView() {
+        setTabLayout()
+    }
+
+    private fun setTabLayout() {
+        initTabNavigation()
+        initTabSelectedListener()
+    }
+
+    private fun initTabNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.leader_space_fcv) as NavHostFragment
+        navHost.findNavController().setGraph(R.navigation.leader_space_nav_graph)
+    }
+
+    private fun initTabSelectedListener() {
+        binding.leaderSpaceTabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                when (tab?.position) {
+                    0 -> findNavController(R.id.leader_space_fcv).navigate(R.id.leaderPlaceFragment)
+                    1 -> findNavController(R.id.leader_space_fcv).navigate(R.id.leaderVoteFragment)
+                    2 -> findNavController(R.id.leader_space_fcv).navigate(R.id.leaderInfoFragment)
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {}
+            override fun onTabReselected(tab: TabLayout.Tab?) {}
+        })
+    }
 
 }
