35379d8e1ca3fc9103beed7bc63e4826f644a1d5
stopkite <stopkite98@gmail.com>
2024-01-25 21:16:51 +0900

    #50 ui: 모임장 모이닷 스페이스 화면 추가

---
+++ app/src/main/java/com/moidot/moidot/presentation/ui/main/group/space/leader/LeaderSpaceActivity.kt
@@ -0,0 +1,9 @@
+package com.moidot.moidot.presentation.ui.main.group.space.leader
+
+import com.moidot.moidot.R
+import com.moidot.moidot.databinding.ActivityLeaderSpaceBinding
+import com.moidot.moidot.presentation.ui.base.BaseActivity
+
+class LeaderSpaceActivity : BaseActivity<ActivityLeaderSpaceBinding>(R.layout.activity_leader_space) {
+
+}
