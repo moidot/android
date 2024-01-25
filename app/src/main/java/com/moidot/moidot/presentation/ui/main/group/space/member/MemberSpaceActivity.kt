d21bd22577f65b3bd4dfda0e7dbe751af464c8c5
stopkite <stopkite98@gmail.com>
2024-01-25 21:23:37 +0900

    #50 ui: 모임원 모이닷 스페이스 화면 추가

---
+++ app/src/main/java/com/moidot/moidot/presentation/ui/main/group/space/member/MemberSpaceActivity.kt
@@ -0,0 +1,9 @@
+package com.moidot.moidot.presentation.ui.main.group.space.member
+
+import com.moidot.moidot.R
+import com.moidot.moidot.databinding.ActivityMemeberSpaceBinding
+import com.moidot.moidot.presentation.ui.base.BaseActivity
+
+class MemberSpaceActivity : BaseActivity<ActivityMemeberSpaceBinding>(R.layout.activity_memeber_space) {
+
+}
