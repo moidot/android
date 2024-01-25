e4189f1e295803de32975d2c6dc92f681d1d3861
stopkite <stopkite98@gmail.com>
2024-01-26 00:32:00 +0900

    #50 fix: Inject constructor 추가 - Hilt 어노테이셔 추가시 명시 안해주면 빌드 오류가 발생한다

--- app/src/main/java/com/moidot/moidot/presentation/ui/main/group/space/leader/viewmodel/LeaderVoteViewModel.kt
+++ app/src/main/java/com/moidot/moidot/presentation/ui/main/group/space/leader/viewmodel/LeaderVoteViewModel.kt
@@ -1,7 +1,9 @@
 package com.moidot.moidot.presentation.ui.main.group.space.leader.viewmodel
 
+import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class LeaderVoteViewModel {
+class LeaderVoteViewModel @Inject constructor(): ViewModel() {
 }
