4504246f33a7e4a1c79c5a02a61a1bff7257d1d8
stopkite <stopkite98@gmail.com>
2024-02-02 22:33:45 +0900

    #77 ui: 모임장소 디폴트 뷰 생성

---
+++ app/src/main/java/com/moidot/moidot/presentation/main/group/space/leader/place/view/LeaderDefaultPlaceFragment.kt
@@ -0,0 +1,13 @@
+package com.moidot.moidot.presentation.main.group.space.leader.place.view
+
+import com.moidot.moidot.R
+import com.moidot.moidot.databinding.FragmentLeaderDefaultPlaceBinding
+import com.moidot.moidot.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LeaderDefaultPlaceFragment : BaseFragment<FragmentLeaderDefaultPlaceBinding>(R.layout.fragment_leader_default_place) {
+
+
+
+}
