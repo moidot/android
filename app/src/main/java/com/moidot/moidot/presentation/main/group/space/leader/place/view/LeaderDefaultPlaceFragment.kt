5194b83fb1ac1d7348d9c29d0915007da6c25e8a
stopkite <stopkite98@gmail.com>
2024-02-03 02:00:02 +0900

    #77 feat: 중간 접힙 상태 구현 및 상태에 따른 이미지 뷰 visible 여부 구현 - behavior_fitToContents 속성 이용 - STATE_HALF_EXPANDED 사용

--- app/src/main/java/com/moidot/moidot/presentation/main/group/space/leader/place/view/LeaderDefaultPlaceFragment.kt
+++ app/src/main/java/com/moidot/moidot/presentation/main/group/space/leader/place/view/LeaderDefaultPlaceFragment.kt
@@ -1,6 +1,7 @@
 package com.moidot.moidot.presentation.main.group.space.leader.place.view
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.core.view.isVisible
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -26,12 +27,18 @@ class LeaderDefaultPlaceFragment : BaseFragment<FragmentLeaderDefaultPlaceBindin
      * 절반 높이인 0.5보다 작거나 같을 때는 (바텀시트의 높이 * slideOffset) 와 1의 값 중 큰 값을 높이로 설정해준다.
      * 절반 보다 클 때는 바텀시트의 절반 높이 만큼을 높이로 설정해준다.*/
     private fun initBottomSheetBehavior() {
+        val emptyMemberContainerView = binding.includeBottomLeaderDefaultPlace.bottomLeaderDefaultPlaceContainerEmptyMemeber
         BottomSheetBehavior.from(binding.fgLeaderDefaultPlaceBottomSheet).apply {
             addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
                 override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    when (newState) {
+                        BottomSheetBehavior.STATE_EXPANDED -> { emptyMemberContainerView.isVisible = true }
+                        BottomSheetBehavior.STATE_HALF_EXPANDED -> { emptyMemberContainerView.isVisible = false }
+                    }
                 }
 
                 override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
                     val interactionView = binding.fgLeaderDefaultPlaceViewInteraction
                     interactionView.layoutParams = interactionView.layoutParams.apply {
                         height = if (slideOffset <= 0.5f) {
