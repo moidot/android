78c6a1d5a6efda7280f4f525fb7a0ae44bbfa1e1
stopkite <stopkite98@gmail.com>
2024-02-05 02:49:05 +0900

    #80 ui: 공통 화면 통일

---
+++ app/src/main/java/com/moidot/moidot/presentation/main/group/space/common/GroupPlaceFragment.kt
@@ -0,0 +1,78 @@
+package com.moidot.moidot.presentation.main.group.space.common
+
+import android.os.Bundle
+import android.view.View
+import androidx.core.view.isVisible
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.kakao.vectormap.GestureType
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.LatLng
+import com.moidot.moidot.R
+import com.moidot.moidot.databinding.FragmentGroupPlaceBinding
+import com.moidot.moidot.presentation.base.BaseFragment
+import com.moidot.moidot.util.view.getScreenHeight
+import kotlin.math.max
+
+class GroupPlaceFragment: BaseFragment<FragmentGroupPlaceBinding>(R.layout.fragment_group_place) {
+
+    private lateinit var kakaoMap: KakaoMap
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initMapView()
+        initView()
+    }
+
+    private fun initMapView() {
+        binding.fgGroupPlaceMapView.layoutParams.height = getScreenHeight(requireContext())
+        binding.fgGroupPlaceMapView.start(object : KakaoMapReadyCallback() {
+            override fun getPosition(): LatLng { // TODO 모임 중심위치 정보
+                return LatLng.from(37.4005, 127.1101)
+            }
+
+            override fun onMapReady(map: KakaoMap) {
+                kakaoMap = map
+                disableGestures()
+            }
+        })
+    }
+
+    private fun disableGestures() {
+        GestureType.values().forEach { kakaoMap.setGestureEnable(it, false) }
+    }
+
+    private fun initView() {
+        initBottomSheetBehavior()
+    }
+
+    private fun initBottomSheetBehavior() {
+        val interactionView = binding.fgGroupPlaceViewInteraction
+        val emptyMemberContainerView = binding.includeBottomLeaderDefaultPlace.bottomLeaderDefaultPlaceContainerEmptyMemeber
+        BottomSheetBehavior.from(binding.fgGroupPlaceBottomSheet).apply {
+            addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    when (newState) {
+                        BottomSheetBehavior.STATE_EXPANDED -> {
+                            emptyMemberContainerView.isVisible = true
+                        }
+
+                        BottomSheetBehavior.STATE_HALF_EXPANDED -> {
+                            emptyMemberContainerView.isVisible = false
+                        }
+                    }
+                }
+
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                    interactionView.layoutParams = interactionView.layoutParams.apply {
+                        height = if (slideOffset <= 0.5f) {
+                            max((bottomSheet.height * slideOffset).toInt(), 1)
+                        } else {
+                            (bottomSheet.height * 0.5f).toInt()
+                        }
+                    }
+                }
+            })
+        }
+    }
+}
