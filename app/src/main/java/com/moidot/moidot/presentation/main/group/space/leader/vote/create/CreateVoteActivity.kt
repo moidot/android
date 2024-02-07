be61232e108dd5d9562d7f2d0b849c97b56ed0a4
stopkite <stopkite98@gmail.com>
2024-02-07 10:08:14 +0900

    #85 feat: 투표 만들기 액티비티 생성

---
+++ app/src/main/java/com/moidot/moidot/presentation/main/group/space/leader/vote/create/CreateVoteActivity.kt
@@ -0,0 +1,8 @@
+package com.moidot.moidot.presentation.main.group.space.leader.vote.create
+
+import com.moidot.moidot.R
+import com.moidot.moidot.databinding.ActivityCreateVoteBinding
+import com.moidot.moidot.presentation.base.BaseActivity
+
+class CreateVoteActivity:BaseActivity<ActivityCreateVoteBinding>(R.layout.activity_create_vote) {
+}
