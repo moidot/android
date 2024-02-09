6b1a9780dda7abef6210de2fcb076d782bef787e
stopkite <stopkite98@gmail.com>
2024-02-09 19:10:23 +0900

    #93 ui: 모임원 vote 뷰 추가

--- app/src/main/java/com/moidot/moidot/presentation/main/group/space/member/vote/before/MemberVoteBeforeFragment.kt
+++ app/src/main/java/com/moidot/moidot/presentation/main/group/space/member/vote/before/MemberVoteBeforeFragment.kt
@@ -1,10 +1,38 @@
 package com.moidot.moidot.presentation.main.group.space.member.vote.before
 
+import android.os.Bundle
+import android.view.View
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.LatLng
 import com.moidot.moidot.R
 import com.moidot.moidot.databinding.FragmentMemberVoteBeforeBinding
 import com.moidot.moidot.presentation.base.BaseFragment
+import com.moidot.moidot.util.MarkerManager
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MemberVoteBeforeFragment: BaseFragment<FragmentMemberVoteBeforeBinding>(R.layout.fragment_member_vote_before) {
+class MemberVoteBeforeFragment : BaseFragment<FragmentMemberVoteBeforeBinding>(R.layout.fragment_member_vote_before) {
+
+    private lateinit var kakaoMap: KakaoMap
+    private lateinit var mapManager: MarkerManager
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initMapView()
+    }
+
+    private fun initMapView() {
+        mapManager = MarkerManager(requireContext())
+        binding.fgMemberVoteBeforeMapView.start(object : KakaoMapReadyCallback() {
+            override fun getPosition(): LatLng {
+                return LatLng.from(37.4005, 127.1101)
+            }
+
+            override fun onMapReady(map: KakaoMap) {
+                kakaoMap = map
+            }
+        })
+    }
+
 }
