8a16716ef48b235b51f1a455dc7a08a375e4d724
stopkite <stopkite98@gmail.com>
2024-02-12 02:37:54 +0900

    #100 style: string 공통 변수명 변경

---
+++ app/src/main/java/com/moidot/moidot/presentation/main/group/space/member/vote/finish/MemberVoteFinishViewModel.kt
@@ -0,0 +1,8 @@
+package com.moidot.moidot.presentation.main.group.space.member.vote.finish
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class MemberVoteFinishViewModel: ViewModel() {
+}
