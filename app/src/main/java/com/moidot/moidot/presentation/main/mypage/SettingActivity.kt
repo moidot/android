dcc64dc56e7d66f1a6eceb66df071993990dada4
stopkite <stopkite98@gmail.com>
2024-02-20 21:53:29 +0900

    #105 ui: 모임 탈퇴 다이얼로그 추가

--- app/src/main/java/com/moidot/moidot/presentation/main/mypage/SettingActivity.kt
+++ app/src/main/java/com/moidot/moidot/presentation/main/mypage/SettingActivity.kt
@@ -1,9 +1,39 @@
 package com.moidot.moidot.presentation.main.mypage
 
+import android.os.Bundle
+import androidx.activity.viewModels
 import com.moidot.moidot.R
 import com.moidot.moidot.databinding.ActivitySettingBinding
 import com.moidot.moidot.presentation.base.BaseActivity
+import com.moidot.moidot.util.popup.PopupTwoButtonDialog
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SettingActivity : BaseActivity<ActivitySettingBinding>(R.layout.activity_setting) {
+
+    private val viewModel: SettingViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initBinding()
+    }
+
+    private fun initBinding() {
+        binding.activity = this
+        binding.viewModel = viewModel
+    }
+
+    fun onClickLogout() {
+        viewModel.logout()
+    }
+
+    fun onClickWithdrawal() {
+        PopupTwoButtonDialog(
+            this,
+            getString(R.string.setting_withdrawal_dialog_title),
+            getString(R.string.setting_withdrawal_dialog_content),
+            getString(R.string.setting_withdrawal_dialog_btn)
+        ) { }.show()
+    }
 
 }
---
+++ app/src/main/java/com/moidot/moidot/presentation/main/mypage/SettingViewModel.kt
@@ -0,0 +1,13 @@
+package com.moidot.moidot.presentation.main.mypage
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class SettingViewModel @Inject constructor() : ViewModel() {
+
+    fun logout() {
+
+    }
+}
