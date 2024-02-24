974583ac7541245bc2358044e03f66b9dbfe31d4
stopkite <stopkite98@gmail.com>
2024-02-25 00:22:59 +0900

    #116 ui: 모임장 투표 시작 전 로직 구현

---
+++ app/src/main/java/com/moidot/moidot/presentation/main/group/space/leader/vote/before/LeaderVoteBeforeViewModel.kt
@@ -0,0 +1,34 @@
+package com.moidot.moidot.presentation.main.group.space.leader.vote.before
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.moidot.moidot.data.remote.response.ResponseBestRegion
+import com.moidot.moidot.repository.GroupPlaceRepository
+import com.moidot.moidot.util.event.MutableSingleLiveData
+import com.moidot.moidot.util.event.SingleLiveData
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LeaderVoteBeforeViewModel @Inject constructor(private val groupPlaceRepository: GroupPlaceRepository) : ViewModel() {
+
+    private val _bestRegions = MutableLiveData<List<ResponseBestRegion.Data>>()
+    val bestRegions: LiveData<List<ResponseBestRegion.Data>> = _bestRegions
+
+    private val _showToastEvent = MutableSingleLiveData<String>()
+    val showToastEvent: SingleLiveData<String> = _showToastEvent
+
+    fun loadBestRegions(groupId: Int) {
+        viewModelScope.launch {
+            groupPlaceRepository.bestRegions(groupId).onSuccess {
+                if (it.code == 0) _bestRegions.value = it.data
+                else _showToastEvent.setValue(it.message.toString())
+            }.onFailure {
+                _showToastEvent.setValue(it.message.toString())
+            }
+        }
+    }
+}
